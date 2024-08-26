5923fd06986ce14d3e169084004546bf5528fb02
Oualid Zari <zari@explorer.eurecom.fr>
2024-08-27 00:30:33 +0200

    Revert "table 4 results reporduction done"
    This reverts commit a8aa0e415150ee0f50a22f4bf20fa31e46db0693.

--- scripts/run_table4.sh
+++ scripts/run_table4.sh
@@ -7,18 +7,18 @@ function run_experiment() {
     dataset=$3
     architecture=$4
 
-    # Change to the parent directory before running the Python script
-    (cd .. && python main.py --dataset ${dataset} --device cuda --gpu 3 --seed ${seed} --lr 0.01 --epochs 300 --val_ratio 0.02 \
+
+    python main.py --dataset ${dataset} --device cuda --gpu 3 --seed ${seed} --lr 0.01 --epochs 300 --val_ratio 0.02 \
         --train_ratio 0.5 \
         --fraction_data_gcn ${fraction_data_gcn} \
         --architecture ${architecture} \
         --perform_attack_all_methods --attack_all_nodes \
         --attack_methods 'labels,features,gradients,output_server,forward_values' \
-        --local_logging \
-        --experiment_name "Table 4 results Cora" \
-        --attack_epochs [-1])
-}
+        --use_wandb \
+        --experiment_comment "Table 4 results Cora" \
+        --attack_epochs [-1] \
 
+}
 export -f run_experiment
 seeds=(42 12 36 15 11 99 04 09 98 10)
 fraction_data_gcn=(0.5)
