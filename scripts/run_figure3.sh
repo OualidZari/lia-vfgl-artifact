cc08c0c8df3978641ace1cfca01b8c8a82991c4a
Oualid Zari <zari@explorer.eurecom.fr>
2024-08-27 01:40:43 +0200

    fixed plot for Figure 3, needs more styling

--- scripts/run_figure3.sh
+++ scripts/run_figure3.sh
@@ -15,7 +15,7 @@ function run_experiment() {
         --perform_attack_all_methods --attack_all_nodes \
         --attack_methods 'labels,features,gradients,output_server,forward_values' \
         --local_logging \
-        --experiment_name "Table 4 results Cora" \
+        --experiment_name "Figure 3 results Cora" \
         --attack_epochs [-1])
 }
 
