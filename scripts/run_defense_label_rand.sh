95a0863acba6ae358aa0c3d3fc9d19af81e501e7
Oualid Zari <zari@explorer.eurecom.fr>
2024-08-28 03:12:53 +0200

    added readme file

--- scripts/run_defense_label_rand.sh
+++ scripts/run_defense_label_rand.sh
@@ -29,5 +29,4 @@ architectures=('gcn')
 datasets=('Cora')
 label_defense_budgets=(0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 num_runs=10
-
 parallel --line-buffer -j ${num_runs} run_experiment ::: "${seeds[@]}" ::: "${fraction_data_gcn[@]}" ::: "${datasets[@]}" ::: "${architectures[@]}" ::: "${label_defense_budgets[@]}"
